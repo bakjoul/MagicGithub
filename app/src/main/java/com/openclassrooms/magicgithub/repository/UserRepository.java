c02917450cc54de88a13aaab7ff46cc8c8697e58
bakjoul <bakjoul@gmail.com>
2021-12-13 15:58:45 +0100

    Commentaires et réinsertions des TODOs pour les retrouver rapidement

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -1,28 +1,30 @@
 package com.openclassrooms.magicgithub.repository;
 
 import com.openclassrooms.magicgithub.api.ApiService;
-import com.openclassrooms.magicgithub.api.FakeApiService;
 import com.openclassrooms.magicgithub.model.User;
 
 import java.util.List;
 
 public class UserRepository {
 
-    private final ApiService apiService;
+    private final ApiService apiService; // TODO: A utiliser
 
     public UserRepository(ApiService apiService) {
         this.apiService = apiService;
     }
 
     public List<User> getUsers() {
+        // TODO: Retourne le résultat de getUsers()
         return apiService.getUsers();
     }
 
     public void generateRandomUser() {
+        // TODO: Appelle generateRandomUser()
         apiService.generateRandomUser();
     }
 
     public void deleteUser(User user) {
+        // TODO: Appelle deleteUser()
         apiService.deleteUser(user);
     }
 }
